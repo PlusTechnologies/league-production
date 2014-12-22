7cbadb515cd6b38a3e1e09e9250a3faebf0697b3
David Hernandez <davidh@plusconsulting.co>
2014-12-22 16:25:25 -0600

    fix classes

--- app/models/Player.php
+++ app/models/Player.php
@@ -15,7 +15,7 @@ class Player extends \Eloquent {
 
 	public function user()
   {
-        return $this->belongsTo('user');
+        return $this->belongsTo('User');
   }
   public function setDobAttribute($value)
   {
