2af1116ff66ca31ef743a5fbf90e848c705b05ed
David Hernandez <davidh@plusconsulting.co>
2015-03-17 21:48:51 -0500

    remove twilio auth token

--- app/config/services.php
+++ app/config/services.php
@@ -32,7 +32,7 @@ return array(
         'connections' => array(
             'twilio' => array(
                 'sid' => 'ACacbe6304c2950978167b82501cf19512',
-                'token' => '0d8689550b4c6e9d259e57e76bf72a6d',
+                'token' => getenv("TK_TW"),
                 'from' => '972-992-1982',
                 'ssl_verify' => true,
             ),
