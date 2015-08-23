35f5c3fc9b128e1addaa5866c55163f426ff080e
David Hernandez <davidh@plusconsulting.co>
2015-08-23 01:36:56 -0500

    enabled sms and emails to be send, debug off

--- app/controllers/EventoController.php
+++ app/controllers/EventoController.php
@@ -382,10 +382,10 @@ public function doAnnouncement($id)
 		foreach ($destination as $recipient) {
 			//send email notification of acceptance queue
 			$data = array('club'=>$club, 'messageOriginal'=>$messageData, 'subject'=>$messageSubject, 'team'=>$event);
-			// Mail::later(3,'emails.announcement.default', $data, function($message) use ($recipient, $club, $messageSubject){
-			// 	$message->to($recipient['email'], $recipient['name'])
-			// 	->subject("$messageSubject | ".$club->name);
-			// });
+			Mail::later(3,'emails.announcement.default', $data, function($message) use ($recipient, $club, $messageSubject){
+				$message->to($recipient['email'], $recipient['name'])
+				->subject("$messageSubject | ".$club->name);
+			});
 			$recipientEmail[] = array(
 				'name'=>$recipient['name'],
 				'email'=>$recipient['email'],
@@ -396,10 +396,10 @@ public function doAnnouncement($id)
 					'mobile'=>$recipient['mobile'],
 				);
 				//queue sms
-				// Queue::push(function($job) use ($recipient, $sms){
-				// 	Twilio::message($recipient['mobile'], $sms);
-				// 	$job->delete();
-				// });
+				Queue::push(function($job) use ($recipient, $sms){
+					Twilio::message($recipient['mobile'], $sms);
+					$job->delete();
+				});
 			}
 		}
 	}
